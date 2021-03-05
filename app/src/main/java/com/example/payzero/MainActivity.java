6671de01a9f1884b245e480177d4510a1b09ff0d
santosh kumar <santoshkumar26330@gmail.com>
2021-03-05 12:59:21 +0530

    done

--- app/src/main/java/com/example/payzero/MainActivity.java
+++ app/src/main/java/com/example/payzero/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.payzero;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
 import android.view.View;
 
@@ -14,12 +16,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+private  CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
 
+    cardView=(CardView)findViewById(R.id.cardphone);
+    cardView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent=new Intent(MainActivity.this,Mobile.class);
+            startActivity(intent);
 
+        }
+    });
     }}
---
+++ app/src/main/java/com/example/payzero/Mobile.java
@@ -0,0 +1,14 @@
+package com.example.payzero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mobile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile);
+    }
+}
